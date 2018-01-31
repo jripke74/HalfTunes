70249a9a2ec59a392746c7b0c891308c697e28ab
Jeff Ripke <jripke74@gmail.com>
2018-01-31 11:03:22 -0800

    added download capabilities

--- HalfTunes/Controller/SearchVC+URLSessionDelegates.swift
+++ HalfTunes/Controller/SearchVC+URLSessionDelegates.swift
@@ -10,6 +10,27 @@ import Foundation
 
 extension SearchViewController: URLSessionDownloadDelegate {
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-        print("Finished downloading to \(location).")
+        // 1
+        guard let sourceURL = downloadTask.originalRequest?.url else { return }
+        let download = downloadService.activeDownloads[sourceURL]
+        downloadService.activeDownloads[sourceURL] = nil
+        // 2
+        let destinationURL = localFilePath(for: sourceURL)
+        print(destinationURL)
+        // 3
+        let fileManager = FileManager.default
+        try? fileManager.removeItem(at: destinationURL)
+        do {
+            try fileManager.copyItem(at: location, to: destinationURL)
+            download?.track.downloaded = true
+        } catch let error {
+            print("Could not copy file to disk: \(error.localizedDescription)")
+        }
+        // 4
+        if let index = download?.track.index {
+            DispatchQueue.main.async {
+                self.tableView.reloadRows(at: [IndexPath(row: index, section: 0)], with: .none)
+            }
+        }
     }
 }
